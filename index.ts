ef112e8cdcbcef423acd101fbd4ae01957f9fc2a
dinh-pham-optimizely <dinh.pham@optimizely.com>
2024-12-30 13:40:45 +0700

    helper to replace component placeholder doesnt work

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ import { Editor, editors } from './lib/editor';
 
 program
   .name('Alloy CLI Frontend')
-  .version('1.1.3')
+  .version('1.1.4')
   .description('Alloy CLI to generate frontend components and more');
 
 program
